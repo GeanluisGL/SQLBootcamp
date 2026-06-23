edf60171e958331fc43056e148446a8c65d282df
Geanluis Lorenzo R. <94375631+GeanluisGL@users.noreply.github.com>
2026-06-22 21:11:59 -0400

    Joins

---
+++ Section 6 SQL Joins/06_JOINs.sql
@@ -0,0 +1,12 @@
+Use MyDatabase
+Go
+
+/** ============================================== **/
+/**SQL Joins **/
+/** ============================================== **/
+
+-- No Join
+-- RETRIVE ALL DATA FROM CUSDTOMERS AND ORDERS IN TWO DIFFERENT RESULTS
+
+Select * FROM customers;
+Select * FROM orders;
