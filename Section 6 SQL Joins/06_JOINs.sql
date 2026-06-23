3d44d3f39cae50aa75e605467ea32e3599d7e56e
Geanluis Lorenzo R. <94375631+GeanluisGL@users.noreply.github.com>
2026-06-22 21:30:50 -0400

    Add files via upload

--- Section 6 SQL Joins/06_JOINs.sql
+++ Section 6 SQL Joins/06_JOINs.sql
@@ -10,3 +10,28 @@ Go
 
 Select * FROM customers;
 Select * FROM orders;
+
+
+--Inner Join
+--Get all customers along with their orders, but only for customer who have placed an order
+
+Select 
+	id,
+	first_name,
+	order_id,
+	sales
+FROM customers
+INNER JOIN orders
+on customers.id = orders.customer_id
+
+/**The column could be repited and the Database could confuse with who is he calling to,
+so this could be solve with the property [Table_name].[column_Name] (and the column name could be abbreviate by using the "AS")**/
+
+Select 
+	c.id,
+	c.first_name,
+	order_id,
+	sales
+FROM customers as c
+INNER JOIN orders
+on c.id = orders.customer_id
