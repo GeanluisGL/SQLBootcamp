1c096c083be713f0aebbf532bad3c94fa34cab8f
Geanluis Lorenzo R. <94375631+GeanluisGL@users.noreply.github.com>
2026-06-24 09:44:52 -0400

    Union all example

--- Section 7 Set Operators/07_SetOperators.sql
+++ Section 7 Set Operators/07_SetOperators.sql
@@ -12,3 +12,15 @@ SELECT
 c.FirstName,
 c.LastName
 FROM sales.Customers as c
+
+--UNION ALL
+/**Combine the data from employees and customers, INCLUDING DUPLICATES**/
+SELECT 
+E.FirstName,
+E.LastName
+FROM sales.Employees as E
+UNION ALL
+SELECT 
+c.FirstName,
+c.LastName
+FROM sales.Customers as c
