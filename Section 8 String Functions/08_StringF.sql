077f10348f3c7f7adb0d5612abdfa60c60b68916
Geanluis Lorenzo R. <94375631+GeanluisGL@users.noreply.github.com>
2026-06-26 00:26:38 -0400

    Upper and Lower case

--- Section 8 String Functions/08_StringF.sql
+++ Section 8 String Functions/08_StringF.sql
@@ -11,3 +11,13 @@ FirstName,
 Country,
 CONCAT(FirstName, ' ', Country ) As 'Name & Country'
 FROM Sales.Customers 
+
+--Upper and Lower
+--Convert the first name to uppercase
+ Select
+FirstName, 
+Country,
+CONCAT(FirstName, ' ', Country ) As 'Name & Country',
+Upper(Firstname) as 'Upper_Case',
+lower(Firstname) as 'Lower_Case'
+FROM Sales.Customers 
