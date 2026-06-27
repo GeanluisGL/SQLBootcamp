4259b431b4c28ec467c9d36f8145982d80fb429f
Geanluis Lorenzo R. <94375631+GeanluisGL@users.noreply.github.com>
2026-06-26 21:57:40 -0400

    Refactor SQL script for clarity and structure

--- Section 9 Date Time Functions/09_TimeStamp.sql
+++ Section 9 Date Time Functions/09_TimeStamp.sql
@@ -1,9 +1,18 @@
 --Section 9 Date Time Functions
 
-
+--Table where we gonna WORK
 Select
 ORderID,
 OrderDate,
 ShipDate,
 CreationTime
 From Sales.orders
+
+-- Day, month, day
+Select
+OrderId,
+CreationTime,
+Year(CreationTime) as 'Year',
+Month(CreationTime) as 'Month',
+Day(CreationTime) as 'Day'
+FROM Sales.Orders
