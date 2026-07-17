f9eb81b30133232639abf2b442fcaa0f5ab3fa57
Geanluis Lorenzo R. <94375631+GeanluisGL@users.noreply.github.com>
2026-07-17 19:43:58 -0400

    Add files via upload

---
+++ Section 10 Null Functions/10_NullsFunction.sql
@@ -0,0 +1,29 @@
+Use SalesDB
+Go
+
+--FIND THE AVERAGE SCORES OF THE CUSTOMERS
+--Coalesce is used for replacing null's values in a column or a row - IN this case is for INT
+SELECT
+sCORE,
+AVG(COALESCE(SCORE, 0)) OVER () AS 'SCORE AVERAGE'
+FROM Sales.Customers
+
+-- IN this case is for a String Value.
+	Select 
+	Lastname,
+	Coalesce(Lastname, 'Unknown') as 'Lastname'
+	FROM Sales.Customers
+
+
+-- USE CASE FOR MATHEMATICAL OPERATOIN
+-- Display the full name of customers by merging their first and last names
+-- And add 10 bonus points to each customer's score
+
+SELECT
+CustomerID,
+FirstName,
+LastName,
+FirstName + ' ' + Coalesce(Lastname, ' ') AS 'Fullname', 
+Score,
+(Coalesce(score, 0)) + 10 AS '+ 10 POINTS'
+FROM Sales.Customers
